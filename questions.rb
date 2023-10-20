e2dcef479f2cd12e450462555e6b56a86d2b925a
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-10-20 09:55:20 -0700

    edit get result method

--- questions.rb
+++ questions.rb
@@ -12,3 +12,19 @@
   }
   
 ]
+
+
+def get_question(id)
+	result = @questions.find{ |question| question[:id]==id}
+	# question = result[:problem]
+	# puts "question #{question}"
+	result
+end
+
+def check_answer(v)
+ if question[:answer]===v
+	 true
+ else 
+	false
+ end
+end
