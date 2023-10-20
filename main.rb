5a959e22555b65747eca8004115077b46419afa4
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-10-20 12:02:41 -0700

    refactor and clean up code

--- main.rb
+++ main.rb
@@ -4,12 +4,12 @@ require './questions'
 
 
 ##created players as global variables
- $p1 = @players[0]
- $p2 = @players[1]
+ $p1 = Player.new('p1')
+ $p2 = Player.new('p2')
 
  # method that shows the game scores
  def show_score
-	 puts "p1 : #{$p1.life}/3 vs p2 : #{$p2.life}/3"
+	 puts "#{$p1.name}: #{$p1.life}/3 vs #{$p2.name}: #{$p2.life}/3"
  end
 
  # method to close the game
@@ -26,20 +26,15 @@ require './questions'
 	puts "#{$current_player.name} select a question number from 1-20 "
 	print '>'
 	
-	# player_1 input
-	choice = $stdin.gets.chomp.to_i
+	# player input
+	input = $stdin.gets.chomp.to_i
 
-	# checking if user input is correct
-	# if !(1..20).include?(choice)
-	# 	puts "enter a valid number"
-	
 	#game gets question
-		question = (get_question(choice))[:problem]
+		question = (get_question(input))[:problem]
 		puts question
 	
-
 	#game stores correct answer
-	correct_answer = get_question(choice)[:answer]
+	correct_answer = get_question(input)[:answer]
 
 	print 'type your answer > : '
 	#player answers
@@ -49,11 +44,12 @@ require './questions'
 
 	if vet
 		puts "✅ Correct!!! great job"
-		puts "#{ $current_player.name} has #{ $current_player.life}/3"
+		puts "#{ $current_player.name} has #{ $current_player.life}/3 lives left"
 	else 
 		puts "❌ such  whooper!!! correct answer is #{correct_answer}"
 		$current_player.lose_life
 		puts "#{ $current_player.name} has #{ $current_player.life}/3 lives left"
+		
 	end
 
 	show_score
@@ -63,8 +59,22 @@ require './questions'
 end
 
 def start_game
- puts "game has started!!!"
- puts "p1 : #{$p1.name} vs p2 : #{$p2.name}"
+ puts "A New Game Has Started!!! 😎"
+
+ # gets players name
+ puts "select a player name for p1"
+		print '>'
+		# player input
+		input = $stdin.gets.chomp
+		$p1.name = input
+
+ puts "select a player name for p2"
+		print '>'
+		# player input
+		input = $stdin.gets.chomp
+		$p2.name = input
+
+ puts "New Match :- p1: #{$p1.name} vs p2: #{$p2.name}"
 
 	while ($p1.life != 0 && $p2.life != 0)
 		$current_player = $p1
